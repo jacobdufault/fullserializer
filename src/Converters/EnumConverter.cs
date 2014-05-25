dcf7049b07f09fe7b2b1d04ad3152d27cd4b402e
unknown <jacobdufault@gmail.com>
2014-05-24 21:02:43 -0500

    Support flags enums

--- src/Converters/EnumConverter.cs
+++ src/Converters/EnumConverter.cs
@@ -20,17 +20,28 @@ namespace FullJson {
         }
 
         public JsonFailure TrySerialize(object instance, out JsonData serialized, Type storageType) {
-            serialized = new JsonData(Enum.GetName(storageType, instance));
+            if (Attribute.IsDefined(storageType, typeof(FlagsAttribute))) {
+                serialized = new JsonData(Convert.ToInt32(instance));
+            }
+            else {
+                serialized = new JsonData(Enum.GetName(storageType, instance));
+            }
             return JsonFailure.Success;
         }
 
         public JsonFailure TryDeserialize(JsonData data, ref object instance, Type storageType) {
-            if (data.IsString == false) return JsonFailure.Fail("Enum converter can only deserialize strings");
+            if (data.IsString) {
+                string enumValue = data.AsString;
+                instance = Enum.Parse(storageType, enumValue);
+                return JsonFailure.Success;
+            }
+            else if (data.IsFloat) {
+                int enumValue = (int)data.AsFloat;
+                instance = Enum.ToObject(storageType, enumValue);
+                return JsonFailure.Success;
+            }
 
-            string enumValue = data.AsString;
-            instance = Enum.Parse(storageType, enumValue);
-
-            return JsonFailure.Success;
+            return JsonFailure.Fail("EnumConverter encountered an unknown JSON data type");
         }
 
         public object CreateInstance(JsonData data, Type storageType) {
