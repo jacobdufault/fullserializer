86e6fb07e3d5ea54af0a5bc19237db0599175bad
Jacob Dufault <jacobdufault@gmail.com>
2014-07-14 17:00:46 +0000

    Add tests for preconstructed object instances

---
+++ Testing/Editor/InitialInstanceTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace FullSerializer.Tests {
+    public class SimpleModel {
+        public int A;
+    }
+
+    public class InitialInstanceTests {
+        [Test]
+        public void TestInitialInstance() {
+            SimpleModel model1 = new SimpleModel { A = 3 };
+
+            fsData data;
+
+            var serializer = new fsSerializer();
+            Assert.IsTrue(serializer.TrySerialize(model1, out data).Succeeded);
+
+            model1.A = 1;
+            SimpleModel model2 = model1;
+            Assert.IsTrue(serializer.TryDeserialize(data, ref model2).Succeeded);
+
+            Assert.AreEqual(model1.A, 3);
+            Assert.AreEqual(model2.A, 3);
+            Assert.IsTrue(ReferenceEquals(model1, model2));
+        }
+    }
+}
