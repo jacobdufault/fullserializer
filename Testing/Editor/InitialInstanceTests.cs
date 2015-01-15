53ed864513ca058343d7d49a7b5ed200c5a2240a
Jacob Dufault <jacobdufault@gmail.com>
2015-01-15 03:40:19 -0500

    Add test description and fix param order in Assert call

--- Testing/Editor/InitialInstanceTests.cs
+++ Testing/Editor/InitialInstanceTests.cs
@@ -7,7 +7,11 @@ namespace FullSerializer.Tests.InitialInstance {
 
     public class InitialInstanceTests {
         [Test]
-        public void TestInitialInstance() {
+        public void TestPopulateObject() {
+            // This test verifies that when we pass in an existing object
+            // instance that same instance is used to deserialize into, ie,
+            // we can do the equivalent of Json.NET's PopulateObject
+
             SimpleModel model1 = new SimpleModel { A = 3 };
 
             fsData data;
@@ -17,10 +21,13 @@ namespace FullSerializer.Tests.InitialInstance {
 
             model1.A = 1;
             SimpleModel model2 = model1;
+            Assert.AreEqual(1, model1.A);
+            Assert.AreEqual(1, model2.A);
+
             Assert.IsTrue(serializer.TryDeserialize(data, ref model2).Succeeded);
 
-            Assert.AreEqual(model1.A, 3);
-            Assert.AreEqual(model2.A, 3);
+            Assert.AreEqual(3, model1.A);
+            Assert.AreEqual(3, model2.A);
             Assert.IsTrue(ReferenceEquals(model1, model2));
         }
     }
