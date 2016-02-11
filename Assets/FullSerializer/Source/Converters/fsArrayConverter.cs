5757870c5b55c8e19fcf5119a60a375776bc5d24
Ben Hymers <b.hymers@gmail.com>
2016-02-11 18:37:45 +0000

    Support 2D array serialization
    Add unit tests to demonstrate 1D and 2D array serialization
    
    Fix #80

--- Assets/FullSerializer/Source/Converters/fsArrayConverter.cs
+++ Assets/FullSerializer/Source/Converters/fsArrayConverter.cs
@@ -4,7 +4,7 @@ using System.Collections;
 namespace FullSerializer.Internal {
     public class fsArrayConverter : fsConverter {
         public override bool CanProcess(Type type) {
-            return type.IsArray;
+            return type.IsArray && type.GetArrayRank() == 1;
         }
 
         public override bool RequestCycleSupport(Type storageType) {
@@ -77,4 +77,130 @@ namespace FullSerializer.Internal {
             return fsMetaType.Get(Serializer.Config, storageType).CreateInstance();
         }
     }
+
+    public class fs2DArrayConverter : fsConverter
+    {
+        public override bool CanProcess(Type type)
+        {
+            return type.IsArray && type.GetArrayRank() == 2;
+        }
+
+        public override bool RequestCycleSupport(Type storageType)
+        {
+            return false;
+        }
+
+        public override bool RequestInheritanceSupport(Type storageType)
+        {
+            return false;
+        }
+
+        public override fsResult TrySerialize(object instance, out fsData serialized, Type storageType)
+        {
+            var asArray = (Array)instance;
+            IList asIList = asArray;
+
+            Type elementType = storageType.GetElementType();
+
+            var result = fsResult.Success;
+
+            serialized = fsData.CreateDictionary();
+            var serializedDict = serialized.AsDictionary;
+            var serializedListData = fsData.CreateList(asIList.Count);
+            serializedDict.Add("c", new fsData(asArray.GetLength(1)));
+            serializedDict.Add("r", new fsData(asArray.GetLength(0)));
+            serializedDict.Add("a", serializedListData);
+            var serializedList = serializedListData.AsList;
+
+            for(int row = 0; row < asArray.GetLength(0); ++row)
+            {
+                for(int column = 0; column < asArray.GetLength(1); ++column)
+                {
+                    object item = asArray.GetValue(row, column);
+
+                    fsData serializedItem;
+
+                    var itemResult = Serializer.TrySerialize(elementType, item, out serializedItem);
+                    result.AddMessages(itemResult);
+                    if(itemResult.Failed) continue;
+
+                    serializedList.Add(serializedItem);
+                }
+            }
+
+            return result;
+        }
+
+        public override fsResult TryDeserialize(fsData data, ref object instance, Type storageType)
+        {
+            var result = fsResult.Success;
+
+            // Verify that we actually have an Object
+            if((result += CheckType(data, fsDataType.Object)).Failed)
+            {
+                return result;
+            }
+
+            Type elementType = storageType.GetElementType();
+
+            var serializedDict = data.AsDictionary;
+            int columns;
+            if((result += DeserializeMember(serializedDict, null, "c", out columns)).Failed)
+            {
+                return result;
+            }
+
+            int rows;
+            if((result += DeserializeMember(serializedDict, null, "r", out rows)).Failed)
+            {
+                return result;
+            }
+
+            fsData flatList;
+            if(!serializedDict.TryGetValue("a", out flatList))
+            {
+                result.AddMessage("Failed to get flattened list");
+                return result;
+            }
+
+            // Verify that we actually have a List
+            if((result += CheckType(flatList, fsDataType.Array)).Failed)
+            {
+                return result;
+            }
+
+            var array = Array.CreateInstance(elementType, rows, columns);
+            var serializedList = flatList.AsList;
+
+            if(columns * rows > serializedList.Count)
+            {
+                result.AddMessage("Serialised list has more items than can fit in multidimensional array");
+            }
+
+            for(int row = 0; row < rows; ++row)
+            {
+                for(int column = 0; column < columns; ++column)
+                {
+                    var serializedItem = serializedList[column + row * columns];
+                    object deserialized = null;
+
+                    var itemResult = Serializer.TryDeserialize(serializedItem, elementType, ref deserialized);
+                    result.AddMessages(itemResult);
+                    if(itemResult.Failed) continue;
+
+                    // Set the value as if it's a flat array.
+                    array.SetValue(deserialized, row, column);
+                }
+            }
+
+            instance = array;
+            return result;
+        }
+
+        public override object CreateInstance(fsData data, Type storageType)
+        {
+            return fsMetaType.Get(Serializer.Config, storageType).CreateInstance();
+        }
+    }
+
 }
---
+++ Assets/FullSerializer/Testing/Editor/ArrayTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace FullSerializer.Tests.ArrayTest {
+    public class ArrayTests {
+        [Test]
+        public void TestArray1D()
+        {
+            int[] before = new int[ 10 ];
+            DoTest( before );
+        }
+
+        [Test]
+        public void TestArray2D()
+        {
+            int[,] before = new int[ 10,20 ];
+            DoTest( before );
+        }
+
+
+        private void DoTest<T>( T expected ) {
+            fsData serializedData;
+            new fsSerializer().TrySerialize( expected, out serializedData ).AssertSuccessWithoutWarnings();
+
+            var actual = default( T );
+            new fsSerializer().TryDeserialize( serializedData, ref actual );
+
+            Assert.AreEqual( expected, actual );
+        }
+    }
+}
