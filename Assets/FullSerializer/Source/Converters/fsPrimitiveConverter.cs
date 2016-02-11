5fc0626a09642c605ae4d2474b73b0ed84606216
Ben Hymers <b.hymers@gmail.com>
2016-02-11 18:37:19 +0000

    Fix exception when serializing float.NaN, and float infinities
    Add tests to demonstrate fix
    
    Fix #83

--- Assets/FullSerializer/Source/Converters/fsPrimitiveConverter.cs
+++ Assets/FullSerializer/Source/Converters/fsPrimitiveConverter.cs
@@ -61,8 +61,12 @@ namespace FullSerializer.Internal {
                 // Casting from float to double introduces floating point jitter, ie, 0.1 becomes 0.100000001490116.
                 // Casting to decimal as an intermediate step removes the jitter. Not sure why.
                 if (instance.GetType() == typeof(float) &&
-                    // Decimal can't store float.MinValue/float.MaxValue - an exception gets thrown in that scenario.
-                    (float)instance != float.MinValue && (float)instance != float.MaxValue) {
+                    // Decimal can't store float.MinValue/float.MaxValue/float.PositiveInfinity/float.NegativeInfinity/float.NaN - an exception gets thrown in that scenario.
+                    (float)instance != float.MinValue &&
+                    (float)instance != float.MaxValue &&
+                    !float.IsInfinity((float)instance) &&
+                    !float.IsNaN((float)instance)
+                    ) {
                     serialized = new fsData((double)(decimal)(float)instance);
                     return fsResult.Success;
                 }
