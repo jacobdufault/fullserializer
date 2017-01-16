dfda7286a89412bbc2a79d04e1d10b31ff066f1e
Ben Hymers <b.hymers@gmail.com>
2017-01-16 12:48:25 +0000

    Allow fsIgnore to be applied to classes and structs to force all instances of those types to be ignored

---
+++ Assets/FullSerializer/Testing/Editor/IgnoreTypeTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace FullSerializer.Tests {
+    public class IgnoreTypeTests {
+        [fsIgnore]
+        private class IgnoredClass {
+        }
+        private class NotIgnoredClass {
+        }
+        [fsIgnore]
+        private struct IgnoredStruct {
+        }
+        private struct NotIgnoredStruct {
+        }
+        private class Model {
+            public IgnoredClass IgnoredClass;
+            public NotIgnoredClass NotIgnoredClass;
+            public IgnoredStruct IgnoredStruct;
+            public NotIgnoredStruct NotIgnoredStruct;
+        }
+
+        [Test]
+        public void TestSerializeReadOnlyProperty() {
+            var model = new Model();
+
+            fsData data;
+
+            var serializer = new fsSerializer();
+            Assert.IsTrue(serializer.TrySerialize(model, out data).Succeeded);
+
+            var expected = fsData.CreateDictionary();
+            expected.AsDictionary["NotIgnoredClass"] = new fsData();
+            expected.AsDictionary["NotIgnoredStruct"] = fsData.CreateDictionary();
+            Assert.AreEqual(expected, data);
+        }
+    }
+}
